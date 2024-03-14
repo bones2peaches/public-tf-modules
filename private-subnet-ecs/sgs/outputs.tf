ffa09202f17b1b6bb2707153f890545084bd5014
bones2peaches <dylanmarsella1@gmail.com>
2024-03-14 04:49:03 -0400

    refactor

--- private-subnet-ecs/sgs/outputs.tf
+++ private-subnet-ecs/sgs/outputs.tf
@@ -1,3 +1,16 @@
 output "vpce_sg_id" {
   value = aws_security_group.vpce.id
 }
+
+output "rds_sg_id" {
+  value = aws_security_group.rds.id
+}
+
+output "lb_sg_id" {
+  value = aws_security_group.lb.id
+}
+
+
+output "lb_svc_id" {
+  value = aws_security_group.svc.id
+}
