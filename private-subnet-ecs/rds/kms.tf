916f2446df90dfeb650c837b17ffc2623f253e74
bones2peaches <dylanmarsella1@gmail.com>
2024-03-14 06:21:18 -0400

    changhed postgres to rds

--- private-subnet-ecs/rds/kms.tf
+++ private-subnet-ecs/rds/kms.tf
@@ -1,5 +1,19 @@
 data "aws_iam_policy_document" "kms" {
 
+
+  statement {
+    effect = "Allow"
+    actions = ["kms:*"]
+    resources = ["*"]
+    principals {
+           type = "AWS"
+     identifiers =["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"  , "${data.aws_caller_identity.current.arn}" ]
+     }
+  }
+
+
+
+
   statement {
     principals {
      type = "AWS"
@@ -11,20 +25,9 @@ data "aws_iam_policy_document" "kms" {
       variable = "kms:GrantIsForAWSResource"
     }      
     
-    actions   = [ "kms:Create*",
-                "kms:Describe*",
-                "kms:Enable*",
-                "kms:List*",
-                "kms:Put*",
-                "kms:Update*",
-                "kms:Revoke*",
-                "kms:Disable*",
-                "kms:Get*",
-                "kms:Delete*",
-                "kms:TagResource",
-                "kms:UntagResource",
-                "kms:ScheduleKeyDeletion",
-                "kms:CancelKeyDeletion"]
+    actions   = [                 "kms:CreateGrant",
+                "kms:ListGrants",
+                "kms:RevokeGrant"]
     resources = ["*"]
     effect = "Allow"
 
@@ -35,12 +38,10 @@ data "aws_iam_policy_document" "kms" {
      type = "AWS"
      identifiers =var.iam_roles
       }
-    actions   = ["kms:Encrypt",
+    actions   = [                "kms:Encrypt",
                 "kms:Decrypt",
                 "kms:ReEncrypt*",
                 "kms:GenerateDataKey*",
-                "kms:CreateGrant",
-                "kms:ListGrants",
                 "kms:DescribeKey"]
     resources = ["*"]
     effect = "Allow"
@@ -52,3 +53,4 @@ data "aws_iam_policy_document" "kms" {
 resource "aws_kms_key" "this" {
   policy = data.aws_iam_policy_document.kms.json
 }
+
