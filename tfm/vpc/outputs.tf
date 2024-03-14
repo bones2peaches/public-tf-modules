8469882993cfbcb7db0d9e58d394c8dac1589db5
bones2peaches <dylanmarsella1@gmail.com>
2024-03-13 22:09:37 -0400

    fixed output vpc error

--- tfm/vpc/outputs.tf
+++ tfm/vpc/outputs.tf
@@ -1,29 +1,3 @@
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.this.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.this.id
-  }
-
-  tags = {
-    Name = "${var.env}-private"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.this.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
-  }
-
-  tags = {
-    Name = "${var.env}-public"
-  }
-}
-
 resource "aws_route_table_association" "private" {
   count = length(var.private_subnets)
 
